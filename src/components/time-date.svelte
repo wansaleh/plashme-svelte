db32961ec76de30f046f6f18adf596f1c7f3442a
Wan Saleh <wansaleh@gmail.com>
2021-11-07 15:07:26 +0800

    Updates

--- src/components/time-date.svelte
+++ src/components/time-date.svelte
@@ -8,18 +8,26 @@
 </script>
 
 <div
-  class="text-[12vw] font-medium tracking-tight flex items-center text-center leading-none font-clock"
+  class="relative text-[12vw] tracking-tight flex justify-end items-center text-center leading-none font-clock font-bold -mr-2"
 >
   <span class="w-[1.2em] text-right">{format(date, 'hh')}</span>
-  <span class="-mt-10 mx-2.5">:</span>
+
+  <span class="-mt-10 mx-2.5 opacity-25">:</span>
+
   <span class="">{format(date, 'mm')}</span>
-  <span class="-mt-10 mx-2.5">:</span>
-  <span class="w-[1.2em] text-left">{format(date, 'ss')}</span>
-  <span class="font-thin ml-[0.05em]">{format(date, 'a')}</span>
+
+  <span class="-mt-10 mx-2.5 opacity-25">:</span>
+
+  <div class="flex flex-col w-[0.8em] leading-[0.85] tracking-normal">
+    <span class="text-[0.65em]">{format(date, 'ss')}</span>
+    <span class="text-[0.3em] font-thin tracking-widest"
+      >{format(date, 'a')}</span
+    >
+  </div>
 </div>
 
 <div
-  class="mr-0 -mt-4 text-[4vw] font-extralight tracking-normal text-right flex justify-end"
+  class="mr-0 mt-0 text-[4vw] font-extralight tracking-normal text-right flex justify-end"
 >
   {format(date, 'iiii, LLLL d, yyy')}
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,19 +6,18 @@
 </script>
 
 <div
-  class="min-h-screen w-screen flex flex-col justify-center items-center leading-none text-white {process
+  class="relative z-10 min-h-screen w-screen flex flex-col justify-center items-center leading-none text-white {process
     .env.NODE_ENV === 'development'
     ? 'bg'
     : ''}"
 >
-  <div class="main-date">
-    <!-- <div class="w-60"><AnalogClock /></div> -->
+  <div class="absolute inset-0 w-screen h-screen z-0 dark-gradient" />
+
+  <div class="relative z-10">
     <TimeDate />
 
-    <div class="flex items-end justify-end mt-8">
+    <div class="flex items-center justify-end mt-8">
       <Fixtures />
-      <!-- <div class="w-4" />
-      <Table /> -->
     </div>
   </div>
 </div>
@@ -30,5 +29,13 @@
 
   .main-date {
     text-shadow: 0 0 20px rgba(0, 0, 0, 0.5), 0 0 2px rgba(0, 0, 0, 0.2);
+  }
+
+  .dark-gradient {
+    background: radial-gradient(
+      circle,
+      rgba(0, 0, 0, 0.65) 30%,
+      rgba(0, 0, 0, 0) 200%
+    );
   }
 </style>
